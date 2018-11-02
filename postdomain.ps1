388ea99e5c06c285ab0d5ac1a5173462b23c0efe
romz33 <dromines@asu.edu>
2018-11-01 17:50:46 -0700

    added insturctions on how to run the scrips in PS

--- postdomain.ps1
+++ postdomain.ps1
@@ -1,5 +1,8 @@
 # Run this after the machine reboot and check the output for errors
+# Execuite this script in power shell when domains.ps1 and orginizationalunits.ps1 are complete. 
 
 dcdiag
 Get-Service adws,kdc,netlogon,dns
 Get-smbshare
+
+
